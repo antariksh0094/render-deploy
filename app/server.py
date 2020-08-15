a4be2f50eb6a4f327ab148891d00a0ed3491ca6e
antariksh0094 <59045014+antariksh0094@users.noreply.github.com>
2020-08-15 15:58:54 +0530

    Update server.py
    changed back to "export.pkl" file name

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-F43_3A-R5SnE3_puqCGrZhNhf4XsusB'
-export_file_name = '1-8-20_4cycles_after_lr_find.pkl'
+export_file_name = 'export.pkl'
 
 classes = ['Bacterial Pneumonia','COVID-19','Normal','Other Findings','TB','Viral Pneumonia']
 path = Path(__file__).parent
