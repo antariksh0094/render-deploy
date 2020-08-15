7a6d43a0ec4eb813f9040f7d67b0d162a765f52f
antariksh0094 <59045014+antariksh0094@users.noreply.github.com>
2020-08-15 15:48:36 +0530

    changed to my parameters
    changed the export file url and export file name to my file and model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-F43_3A-R5SnE3_puqCGrZhNhf4XsusB'
+export_file_name = '1-8-20_4cycles_after_lr_find.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Bacterial Pneumonia','COVID-19','Normal','Other Findings','TB','Viral Pneumonia']
 path = Path(__file__).parent
 
 app = Starlette()
